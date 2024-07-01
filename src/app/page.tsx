c887ad43a4742e875b54ed12faf7dc81a151383c
HotShot0901 <contact.hot.shor.1234@gmail.com>
2024-07-01 13:36:43 +0530

    Fixed some errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Map from "../components/Map";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-4 py-16 md:px-12 lg:px-24">
